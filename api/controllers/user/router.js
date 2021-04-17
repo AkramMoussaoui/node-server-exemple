63ba91d0ca395d501754809fe4cd8b5e47e1bb84
Akram Moussaoui <akrammoussaoui14@gmail.com>
2021-04-17 15:28:31 +0100

    change naming

--- api/controllers/user/router.js
+++ api/controllers/user/router.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const userController = require('./controller');
-const todoSchema = require('../../../validators/user');
-const validate = require('../../../validators/validate');
+const express = require("express");
+const userController = require("./controller");
+const userSchema = require("../../../validators/user");
+const validate = require("../../../validators/validate");
 const userRouter = express.Router();
 
-userRouter.get('/:id', userController.getUserByID);
-userRouter.get('/', userController.getUsers);
-userRouter.post('/', validate(todoSchema.postUser), userController.addUser);
-userRouter.put('/:id', userController.updateUser);
-userRouter.patch('/:id', userController.updateActive);
-userRouter.delete('/:id', userController.deleteUser);
+userRouter.get("/:id", userController.getUserByID);
+userRouter.get("/", userController.getUsers);
+userRouter.post("/", validate(userSchema.postUser), userController.addUser);
+userRouter.put("/:id", userController.updateUser);
+userRouter.patch("/:id", userController.updateActive);
+userRouter.delete("/:id", userController.deleteUser);
 
 module.exports = userRouter;
